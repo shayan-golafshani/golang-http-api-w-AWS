8c756e1eb8ba3e4fecb439e5b4efad8bbef2ee03
shayan-golafshani <shayan.golafshani@segment.com>
2022-07-25 14:33:53 -0600

    update employee struct JSON & implement dynamoDB mocking interface

--- store/store.go
+++ store/store.go
@@ -1,15 +1,24 @@
 package store
 
+import "github.com/aws/aws-sdk-go/service/dynamodb"
+
 //WILL MOVE ERROR SOMEWHERE ELSE....
 type Error struct {
 	Status int    `json:"status,omitempty"`
 	Msg    string `json:"msg,omitempty"`
 }
 type Employee struct {
-	Name       string `json:"name,omitempty"`
+	Name       string `json:"employeeName,omitempty"`
 	Email      string `json:"email,omitempty"`
 	EmployeeId string `json:"EmployeeId,omitempty"`
 	City       string `json:"city,omitempty"`
 	Address    string `json:"address,omitempty"`
 	Department string `json:"department,omitempty"`
+}
+
+type SubsetDynamoDb interface {
+	DeleteItem(input *dynamodb.DeleteItemInput) (*dynamodb.DeleteItemOutput, error)
+	UpdateItem(input *dynamodb.UpdateItemInput) (*dynamodb.UpdateItemOutput, error)
+	PutItem(input *dynamodb.PutItemInput) (*dynamodb.PutItemOutput, error)
+	GetItem(input *dynamodb.GetItemInput) (*dynamodb.GetItemOutput, error)
 }
