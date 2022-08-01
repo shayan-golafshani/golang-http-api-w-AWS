a0fdf0eb54933a4a19ce0746bbf1b6a1b63821a8
shayan-golafshani <shayan.golafshani@segment.com>
2022-08-01 08:31:35 -0600

    Dry code with error handler func

--- handlers/postEmployee.go
+++ handlers/postEmployee.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
@@ -18,30 +19,27 @@ type GetOneEmployee struct {
 }
 
 func (s Server) PostEmployee(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("POST EMPLOYEE CALLED")
+	fmt.Println("POST Employee called")
 
 	var newEmployeeUUID = uuid.New()
 	fmt.Println("Employee UUID:", newEmployeeUUID)
 
-	//decode the employee from what's posted.
+	//decode the employee from req
 	var post store.Employee
 
-	err := json.NewDecoder(r.Body).Decode(&post)
-	if err != nil {
-		fmt.Println("POST: You've got request body issues", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(store.Error{Status: 500, Msg: "POST: Unable to decode request body! Try again"})
+	//check you can decode the request
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		store.SendError(w, http.StatusInternalServerError, "POST: Unable to decode request body!", err)
 		return
 	}
+	//check for employee name
 	if post.Name == "" {
-		fmt.Println("POST: Please enter an Employee Name, it's empty.")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(store.Error{Status: 400, Msg: "POST: Reform your request with an employeeName!"})
+		store.SendError(w, http.StatusBadRequest, "POST: Reform your request with an employeeName!",
+			errors.New("post: Please enter an Employee Name, it's empty"))
 		return
 	}
 
+	//setup employee to be created
 	newEmployee := store.Employee{
 		Name:       post.Name,
 		Email:      post.Name + "@twilio.com",
@@ -50,31 +48,31 @@ func (s Server) PostEmployee(w http.ResponseWriter, r *http.Request) {
 		Address:    post.Address,
 		Department: post.Department,
 	}
-	fmt.Println("New employee:", newEmployee)
+	fmt.Println("New employee Info:", newEmployee)
 
 	//store newEmployee to DynamoDB after marshalMapping them
 	dynamoDbEmployee, errMarshalling := dynamodbattribute.MarshalMap(newEmployee)
 	if errMarshalling != nil {
-		fmt.Sprintf("POST: failed to DynamoDB marshal Record, %v", errMarshalling.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(store.Error{Status: 500, Msg: "POST: Unable to MarshallMap your employee! Try again"})
+		store.SendError(w, http.StatusInternalServerError,
+			"POST: Unable to MarshallMap your employee! Try again", errMarshalling)
+		return
 	}
 
-	_, errPutItem := s.Db.PutItem(&dynamodb.PutItemInput{
+	//setup putItemInput to create employee record in DynamoDb
+	setupItem := &dynamodb.PutItemInput{
 		TableName: aws.String(TableName),
 		Item:      dynamoDbEmployee,
-	})
-	if errPutItem != nil {
-		fmt.Sprintf("POST: failed to put Record to DynamoDB, %v", errPutItem.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(store.Error{Status: 500, Msg: "POST: failed to put Record to DynamoDB!"})
 	}
 
-	//send response created!
+	if _, errPutItem := s.Db.PutItem(setupItem); errPutItem != nil {
+		store.SendError(w, http.StatusInternalServerError,
+			"POST: failed to put new Employee Record!", errPutItem)
+		return
+	}
+
+	//POST response created & sent!
 	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "application/json")
 	resp := GetOneEmployee{http.StatusCreated, newEmployee}
 	json.NewEncoder(w).Encode(resp)
+	fmt.Println("Post Employee Successfully Completed")
 }
